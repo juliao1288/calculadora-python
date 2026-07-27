11e3737825ee01155a9d85e76f3c3d8147e3b83d
júlião <julioleandro832@gmail.com>
2026-07-27 19:21:17 -0300

    Inserido funções de soma e subtração.

--- calculadora.py
+++ calculadora.py
@@ -2,14 +2,97 @@
 
 def main():
 
-    print(" -- CALCULADORA -- " +
-          " 1) SOMA " +
-          " 2) SUBTRAÇÃO " +
-          " 3) MULTIPLICAÇÃO " +
-          " 4) DIVISÃO " )
+    d = 0
 
-    print()
-    d = input(" Insira o valor desejado: ")
+    while d != "5":
+        print()
+        print()
+
+        print(" -- CALCULADORA -- " +
+            "\n 1) SOMA " +
+            "\n 2) SUBTRAÇÃO " +
+            "\n 3) MULTIPLICAÇÃO " +
+            "\n 4) DIVISÃO " +
+            "\n 5) SAIR ")
+
+        print()
+        d = input(" Insira o valor desejado: ")
+
+        match d:
+
+            case "1":
+
+                while True:
+                    try:
+                        a = float(input(" Insira um número para realizar a soma: "))
+                        b = float(input(" Insira um outro número para realizar a soma: "))
+                        break
+                    except ValueError:
+                        print(" Valores incorretos, tente novamente.\n")
+
+                resultado = soma(a,b)
+                print(f" {a} + {b} = {resultado}")
+
+            case "2":
+                while True:
+                    try:
+                        a = float(input(" Insira um número para realizar a subtração: "))
+                        b = float(input(" Insira um outro número para realizar a subtração: "))
+                        break
+                    except ValueError:
+                        print(" Valores incorretos, tente novamente.\n")
+
+                resultado = subtração(a,b)
+                print(f" {a} - {b} = {resultado}")
+
+
+            case "5":
+                print(" Você escolheu sair...")
+                print()
+
+            case _:
+                print(" Valor inválido, tente novamente.")
+                print()
+
+                      
+                
+
+
+
+
+
+def soma(a: float, b: float) -> float:
+    """
+    Esta função recebe 2 números qualquer e realiza a soma entre eles.
+    
+    Args:
+        a (float) = Número qualquer que o usuário inseriu.
+        b (float) = Outro número qualquer que o usuário inseriu.
+        
+    Returns:
+        resultado (float) = Soma entre os 2 números que o usuário inseriu.
+    
+    """
+    resultado = a + b
+
+    return resultado
+
+def subtração(a: float, b: float) -> float:
+    """
+    Esta função recebe 2 números qualquer e realiza a subtração entre eles.
+    
+    Args:
+        a (float) = Número qualquer que o usuário inseriu.
+        b (float) = Outro número qualquer que o usuário inseriu.
+        
+    Returns:
+        resultado (float) = Subtração entre os 2 números que o usuário inseriu.
+    
+    """
+    resultado = a - b
+
+    return resultado
+
 
 
 
