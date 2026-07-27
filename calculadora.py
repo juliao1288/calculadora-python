b327056b069b494160507ce9766e1d0223eb0588
júlião <julioleandro832@gmail.com>
2026-07-27 19:01:48 -0300

    Feito alterações iniciais no projeto da calculadora.

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+#Calculadora em Python
+
+def main():
+
+    print(" -- CALCULADORA -- " +
+          " 1) SOMA " +
+          " 2) SUBTRAÇÃO " +
+          " 3) MULTIPLICAÇÃO " +
+          " 4) DIVISÃO " )
+
+    print()
+    d = input(" Insira o valor desejado: ")
+
+
+
+
+if __name__ == "__main__":
+    main()
